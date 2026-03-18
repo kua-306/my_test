10be95a57f8ceffe49bec4eb3cedfe1f0d983dd9
kua-306 <thu3062003@gmail.com>
2026-03-18 13:42:41 +0700

    sua pw

--- api.py
+++ api.py
@@ -87,8 +87,9 @@ async def create_user(user:UserCreate,db : AsyncSession = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='User already exists')
-    user.password = hash_password(user.password)
-    new_user = models.Users(**user.dict())
+    hashed_pw = hash_password(user.password)
+    new_user = models.Users(username=user.username, 
+        password=hashed_pw)
     db.add(new_user)
     # logger.info(f"Da luu user: {new_user.username} vao db")
     await db.commit()
