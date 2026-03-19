dfe2cd3cdbe0de99389afa122076b0fb80b983e9
kua-306 <thu3062003@gmail.com>
2026-03-19 10:07:24 +0700

    getcwd

--- test_ui.py
+++ test_ui.py
@@ -3,9 +3,10 @@ from playwright.sync_api import Page, expect
 
 
 def test_example(page: Page) -> None:
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, "index.html")
-    page.goto(f'file://{file_path}')
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # file_path = os.path.join(current_dir, "index.html")
+    # page.goto(f'file://{file_path}')
+    page.goto(f'file://{os.getcwd()}/index.html')
     page.get_by_role("textbox", name="Username").click()
     page.get_by_role("textbox", name="Username").fill("thune")
     page.get_by_role("textbox", name="Password").click()
