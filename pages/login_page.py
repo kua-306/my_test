8f8fdd73d08c8f7ea5e54eef79618b3b95792ea2
kua-306 <thu3062003@gmail.com>
2026-03-25 13:35:47 +0700

    add login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,22 @@
 from pages.base_page import BasePage
 
-class LoginPage(BasePage):
+class SignupPage(BasePage):
     def __init__(self,page):
         super().__init__(page)
         self.username = page.get_by_role("textbox", name="Username")
         self.password = page.get_by_role("textbox", name="Password")
-        self.login_btn = page.get_by_role("button", name="Đăng ký")
+        self.login_btn = page.get_by_role("button", name="Đăng nhập")
+        self.signup_btn = page.get_by_role("button", name="Đăng ký")
+    def signup(self,username,password):
+        self.username.click()
+        self.username.fill(username)
+        self.password.click()
+        self.password.fill(password)
+        self.login_btn.click()
     def login(self,username,password):
         self.username.click()
         self.username.fill(username)
         self.password.click()
         self.password.fill(password)
         self.login_btn.click()
+
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,4 @@
-from pages.login_page import LoginPage
+from pages.login_page import SignupPage
 from pages.question_page import QuestionPage
 import os
 import random
@@ -7,11 +7,12 @@ from playwright.sync_api import Page, expect
 num = random.randint(0, 9999)
 username = f'account{num}'
 password = 'password'
-def test_login(page:Page):
-    login_page = LoginPage(page)
+def test_signup(page:Page):
+    login_page = SignupPage(page)
     login_page.openurl(f'file://{os.getcwd()}/index.html')
-    login_page.login(username,password)
-    expect(login_page.check("#question-section")).to_be_visible()
+    login_page.signup(username,password)
+    # login_page.login(username,password)
+    expect(login_page.check("#question-section")).to_be_enabled()
 
 
 
